5e8ba78e6fe59b11c3505537043d35262296e9bb
TechnikWeber <36840482+TechnikWeber@users.noreply.github.com>
2018-02-26 09:59:03 +0100

    Add files via upload

---
+++ Lantronix Netzwerk Tool/Lantronix Netzwerk Tool/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace Lantronix_Netzwerk_Tool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "MAC:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.textBox1.Location = new System.Drawing.Point(12, 63);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(107, 20);
+            this.textBox1.TabIndex = 17;
+            this.textBox1.Text = "00-80-A3-BE-2F-";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Gainsboro;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(185, 54);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(153, 34);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "MAC-Suche";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Gainsboro;
+            this.button2.Location = new System.Drawing.Point(57, 387);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(235, 23);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Suche zurücksetzen";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 183);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(326, 199);
+            this.listBox1.TabIndex = 14;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(59, 125);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Gerät(e) gefunden.";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Gainsboro;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(185, 95);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 34);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Vollständige Suche";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Gainsboro;
+            this.button4.Location = new System.Drawing.Point(214, 154);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(124, 23);
+            this.button4.TabIndex = 20;
+            this.button4.Text = "Verbinden";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(331, 13);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "______________________________________________________";
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Gainsboro;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(185, 14);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(153, 34);
+            this.button5.TabIndex = 23;
+            this.button5.Text = "Einfache Suche";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 413);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(331, 13);
+            this.label4.TabIndex = 24;
+            this.label4.Text = "______________________________________________________";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 430);
+            this.progressBar1.Maximum = 100000;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(326, 18);
+            this.progressBar1.TabIndex = 25;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.textBox2.Location = new System.Drawing.Point(12, 118);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(45, 20);
+            this.textBox2.TabIndex = 26;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 164);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(143, 13);
+            this.label5.TabIndex = 27;
+            this.label5.Text = "Liste der Geräte im Netzwerk";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 95);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(132, 17);
+            this.checkBox1.TabIndex = 28;
+            this.checkBox1.Text = "Timeout erhöhen (30s)";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(350, 460);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Lantronix Netzwerk Tool V1.0";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Lantronix Netzwerk Tool/Lantronix Netzwerk Tool/Form1.cs
@@ -0,0 +1,412 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Lantronix_Netzwerk_Tool
+{
+    public partial class Form1 : Form
+    {
+        //Formatierung der Textbox
+        String IPundMAC = "{0,-30}{1, 24}";
+        String IPundMAC2 = "{0,-30}{1, 15}";
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Textbox zu Beginn füllen
+            listBox1.Items.Add(String.Format(IPundMAC, "IP-Adresse", "Hardware-Adresse (MAC)"));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Vollständige Suche
+            textBox2.Clear();
+            Cursor.Current = Cursors.WaitCursor;
+            progressBar1.Increment(-100000);
+            listBox1.Items.Clear();
+            listBox1.Items.Add(String.Format(IPundMAC, "IP-Adresse", "Hardware-Adresse (MAC)"));
+
+            Byte[] data = { 0x00, 0x00, 0x00, 0xF6 };
+
+            int port = 30718;
+
+            // Socket definieren
+            Socket bcSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            //bcSocket.Connect(new IPEndPoint(IPAddress.Broadcast, port));
+
+            // EndPoint definieren bzw. Ziel des Broadcastes
+            IPEndPoint iep = new IPEndPoint(IPAddress.Broadcast, port);
+
+            // Optionen auf den Socket binden
+            bcSocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, 1);
+            int Timeout = 2500;
+
+            if(checkBox1.Checked)
+            {
+                Timeout = 30000;
+            }
+            bcSocket.ReceiveTimeout = Timeout;
+
+            byte[] test = new byte[1024];
+
+            IPEndPoint _sender = new IPEndPoint(IPAddress.Any, 0);
+            EndPoint senderRemote = (EndPoint)_sender;
+
+            int durchlauf = 0;
+            int anzahlg = 0;
+
+            //Broadcasten!
+            while (durchlauf <= 100000)
+            {
+                int ausf = 1;
+                bcSocket.SendTo(data, iep);
+                try
+                {
+                    bcSocket.ReceiveFrom(test, ref senderRemote); //IP steht in senderRemote!
+                }
+                catch
+                {
+                    ausf = 0;
+                    durchlauf = 100000;
+                    listBox1.Items.Clear();
+                    listBox1.Items.Add(String.Format(IPundMAC, "IP-Adresse", "Hardware-Adresse (MAC)"));
+                    listBox1.Items.Add("");
+                    listBox1.Items.Add("");
+                    listBox1.Items.Add("");
+                    listBox1.Items.Add("");
+                    listBox1.Items.Add("FEHLER #01");
+                    listBox1.Items.Add("");
+                    listBox1.Items.Add("Kein Gerät gefunden.");
+                    listBox1.Items.Add("Bitte Verbindung zu Lantronix-Gerät prüfen.");
+                    listBox1.Items.Add("- Timeout erhöhen -");
+
+                }
+                if (ausf == 1)
+                {
+                    string IP1 = senderRemote.ToString();
+                    int stelle = IP1.IndexOf(":");
+
+                    string NewIP1 = IP1.Substring(0, stelle);
+
+                    //Mac aus Datenpaket filtern
+                    string MAC1 = BitConverter.ToString(test);
+                    string NewMac1 = MAC1.Substring(72, 17);
+
+                    //Schauen ob Mac bereits in Liste
+                    var item2 = listBox1.FindString(NewIP1);
+
+                    if (item2 == -1)
+                    {
+                        listBox1.Items.Add(String.Format(IPundMAC2, NewIP1, NewMac1));
+                        this.Refresh();
+                        anzahlg++;
+                    }
+                }
+
+                durchlauf++;
+                this.progressBar1.Increment(1);
+            }
+            bcSocket.Close();
+            Cursor.Current = Cursors.Default;
+            textBox2.Text = anzahlg.ToString();
+        }
+        
+
+
+
+
+
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Hier nur nach MAC suchen
+            this.progressBar1.Increment(-100000);
+            textBox2.Clear();
+
+            Byte[] data = { 0x00, 0x00, 0x00, 0xF6 };
+
+            int port = 30718;
+            //string Antwort;
+
+
+            // Socket definieren
+            Socket bcSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            //bcSocket.Connect(new IPEndPoint(IPAddress.Broadcast, port));
+
+            // EndPoint definieren bzw. Ziel des Broadcastes
+            IPEndPoint iep = new IPEndPoint(IPAddress.Broadcast, port);
+
+            // Optionen auf den Socket binden
+            bcSocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, 1);
+
+            int Timeout = 2500;
+
+            if (checkBox1.Checked)
+            {
+                Timeout = 30000;
+            }
+            bcSocket.ReceiveTimeout = Timeout;
+
+            byte[] test = new byte[1024];
+
+            IPEndPoint _sender = new IPEndPoint(IPAddress.Any, 0);
+            EndPoint senderRemote = (EndPoint)_sender;
+
+            //Gesuchte MAC einlesen
+            string sMAC = textBox1.Text;
+
+            //Abfrage ob Text geändert wurde
+            if (sMAC == "00-80-A3-BE-2F-")
+            {
+                listBox1.Items.Clear();
+                listBox1.Items.Add(String.Format(IPundMAC, "IP-Adresse", "Hardware-Adresse (MAC)"));
+                listBox1.Items.Add("");
+                listBox1.Items.Add("");
+                listBox1.Items.Add("");
+                listBox1.Items.Add("");
+                listBox1.Items.Add("FEHLER #02");
+                listBox1.Items.Add("");
+                listBox1.Items.Add("Keine MAC-Adresse erkannt.");
+                listBox1.Items.Add("Bitte die letzten zwei Ziffern angeben.");
+                listBox1.Items.Add("- Timeout erhöhen -");
+
+            }
+            else
+            {
+                listBox1.Items.Clear();
+                listBox1.Items.Add(String.Format(IPundMAC, "IP-Adresse", "Hardware-Adresse (MAC)"));
+                Cursor.Current = Cursors.WaitCursor;
+                int gefunden = 0;
+                int abbruch = 0;
+
+                //Suche solange bis MAC gefunden wurde
+                while (gefunden == 0)
+                {
+                    int ausf2 = 1;
+                    bcSocket.SendTo(data, iep);
+                    try
+                    {
+                        bcSocket.ReceiveFrom(test, ref senderRemote); //IP steht in senderRemote!
+                    }
+                    catch
+                    {
+                        ausf2 = 0;
+                        gefunden = 1;
+                        listBox1.Items.Clear();
+                        listBox1.Items.Add(String.Format(IPundMAC, "IP-Adresse", "Hardware-Adresse (MAC)"));
+                        listBox1.Items.Add("");
+                        listBox1.Items.Add("");
+                        listBox1.Items.Add("");
+                        listBox1.Items.Add("");
+                        listBox1.Items.Add("FEHLER #03");
+                        listBox1.Items.Add("");
+                        listBox1.Items.Add("Kein Gerät mit der angegeben MAC gefunden.");
+                        listBox1.Items.Add("Bitte Verbindung und MAC prüfen.");
+                        listBox1.Items.Add("(Groß- und Kleinschreibung beachten!)");
+                    }
+
+                    if (ausf2 == 1)
+                    {
+                        string IP2 = senderRemote.ToString();
+
+                        //Port abtrennen nach :
+                        int stelle = IP2.IndexOf(":");
+                        string NewIP2 = IP2.Substring(0, stelle);
+
+                        //Port anzeigen wie folgt:
+                        //int port = Integer.parseInt(s.substring(s.indexOf(':') + 1));
+
+                        //Mac aus Datenpaket filtern
+                        string MAC2 = BitConverter.ToString(test);
+                        string NewMac2 = MAC2.Substring(72, 17);
+
+                        if (sMAC == NewMac2)
+                        {
+                            listBox1.Items.Add(String.Format(IPundMAC2, NewIP2, NewMac2));
+                            gefunden = 1;
+                            this.progressBar1.Increment(100000);
+                            textBox2.Text = "1";
+                        }
+
+                        //Direkt das erste wählen (geht nicht richtig)
+                        //listBox1.SetSelected(1, true);
+                    }
+                    abbruch++;
+                    if(abbruch >= 100000)
+                    {
+                        //Prüfen ob Abbruch nicht besser geht (z.B über länge der Mac)
+                        //Auserdem vorher kleinbuchstaben zu groß wandeln
+                        gefunden = 1;
+                        Cursor.Current = Cursors.Default;
+                        //Vorgabe Text zurücksetzen
+                        textBox1.Text = "00-80-A3-BE-2F-";
+                        listBox1.Items.Clear();
+                        listBox1.Items.Add(String.Format(IPundMAC, "IP-Adresse", "Hardware-Adresse (MAC)"));
+                        listBox1.Items.Add("");
+                        listBox1.Items.Add("");
+                        listBox1.Items.Add("");
+                        listBox1.Items.Add("");
+                        listBox1.Items.Add("FEHLER #05");
+                        listBox1.Items.Add("");
+                        listBox1.Items.Add("Zeitüberschreitung.");
+                        listBox1.Items.Add("MAC ist nicht im Netzwerk vorhanden.");
+                        listBox1.Items.Add("Bitte Verbindung und MAC prüfen.");
+                        listBox1.Items.Add("(Groß- und Kleinschreibung beachten!)");
+                    }
+
+                }
+                // Socket schliessen, nach erfolgreichem Senden des Broadcastes
+                bcSocket.Close();
+                Cursor.Current = Cursors.Default;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //Schauen welche IP ausgewählt wurde und Verbinden
+            string ausw = listBox1.GetItemText(listBox1.SelectedItem);
+                int stelle2 = ausw.IndexOf(" ");
+            if (stelle2 == -1)
+            {
+                listBox1.Items.Clear();
+                listBox1.Items.Add(String.Format(IPundMAC, "IP-Adresse", "Hardware-Adresse (MAC)"));
+                listBox1.Items.Add("");
+                listBox1.Items.Add("");
+                listBox1.Items.Add("");
+                listBox1.Items.Add("");
+                listBox1.Items.Add("FEHLER #04");
+                listBox1.Items.Add("");
+                listBox1.Items.Add("Keine Auswahl erkannt.");
+                listBox1.Items.Add("Bitte IP auswählen und erneut Verbinden.");
+            }
+            else
+            {
+                string Auswahl = ausw.Substring(0, stelle2);
+                string http = Auswahl.Insert(0, "http://");
+
+                //Mit IP verbinden über Browser
+                System.Diagnostics.Process.Start(http);
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Liste löschen
+            listBox1.Items.Clear();
+            listBox1.Items.Add(String.Format(IPundMAC, "IP-Adresse", "Hardware-Adresse (MAC)"));
+            textBox2.Clear();
+            this.progressBar1.Increment(-100000);
+            textBox1.Text = ("00-80-A3-BE-2F-");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //Einfache Suche
+            textBox2.Clear();
+            Cursor.Current = Cursors.WaitCursor;
+            progressBar1.Increment(-100000);
+            listBox1.Items.Clear();
+            listBox1.Items.Add(String.Format(IPundMAC, "IP-Adresse", "Hardware-Adresse (MAC)"));
+
+            Byte[] data = { 0x00, 0x00, 0x00, 0xF6 };
+
+            int port = 30718;
+
+            // Socket definieren
+            Socket bcSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            // EndPoint definieren bzw. Ziel des Broadcastes
+            IPEndPoint iep = new IPEndPoint(IPAddress.Broadcast, port);
+
+            // Optionen auf den Socket binden
+            bcSocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, 1);
+
+            int Timeout = 2500;
+
+            if (checkBox1.Checked)
+            {
+                Timeout = 30000;
+            }
+            bcSocket.ReceiveTimeout = Timeout;
+
+            byte[] test = new byte[1024];
+
+            IPEndPoint _sender = new IPEndPoint(IPAddress.Any, 0);
+            EndPoint senderRemote = (EndPoint)_sender;
+
+            int durchlauf = 0;
+            int anzahlg2 = 0;
+
+            while (durchlauf <= 100)
+            {
+                int ausf = 1;
+                bcSocket.SendTo(data, iep);
+                try
+                {
+                    bcSocket.ReceiveFrom(test, ref senderRemote); //IP steht in senderRemote!
+                }
+                catch
+                {
+                    ausf = 0;
+                    durchlauf = 100;
+                    listBox1.Items.Clear();
+                    listBox1.Items.Add(String.Format(IPundMAC, "IP-Adresse", "Hardware-Adresse (MAC)"));
+                    listBox1.Items.Add("");
+                    listBox1.Items.Add("");
+                    listBox1.Items.Add("");
+                    listBox1.Items.Add("");
+                    listBox1.Items.Add("FEHLER #01");
+                    listBox1.Items.Add("");
+                    listBox1.Items.Add("Kein Gerät gefunden.");
+                    listBox1.Items.Add("Bitte Verbindung zu Lantronix-Gerät prüfen.");
+                    listBox1.Items.Add("- Timeout erhöhen -");                }
+                if (ausf == 1)
+                {
+                    string IP1 = senderRemote.ToString();
+                    int stelle = IP1.IndexOf(":");
+
+                    string NewIP1 = IP1.Substring(0, stelle);
+
+                    //Mac aus Datenpaket filtern
+                    string MAC1 = BitConverter.ToString(test);
+                    string NewMac1 = MAC1.Substring(72, 17);
+
+                    //Schauen ob Mac bereits in Liste
+                    var item2 = listBox1.FindString(NewIP1);
+
+                    this.progressBar1.Increment(10000);
+
+                    if (item2 == -1)
+                    {
+                        listBox1.Items.Add(String.Format(IPundMAC2, NewIP1, NewMac1));
+                        this.Refresh();
+                        anzahlg2++;
+                    }
+                }
+
+                durchlauf++;
+            }
+            bcSocket.Close();
+            Cursor.Current = Cursors.Default;
+            textBox2.Text = anzahlg2.ToString();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
